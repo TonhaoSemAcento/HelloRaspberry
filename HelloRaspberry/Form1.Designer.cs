eea630d2ec6b9ddbd43ba3652c4feebd33db8891
TonhaoSemAcento <tonhaosemacento@gmail.com>
2019-03-09 22:41:17 -0300

    Add project files.

---
+++ HelloRaspberry/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HelloRaspberry
+{
+    partial class frmHello
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHello));
+            this.lblHello = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHello.Location = new System.Drawing.Point(7, 9);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(207, 29);
+            this.lblHello.TabIndex = 0;
+            this.lblHello.Text = "Hello Raspbberry!";
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(12, 51);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(196, 46);
+            this.btnClick.TabIndex = 1;
+            this.btnClick.Text = "&Click Me";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.BtnClick_Click);
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(12, 109);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(0, 29);
+            this.lblText.TabIndex = 2;
+            // 
+            // frmHello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.lblHello);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmHello";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello, Raspberry!";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHello;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
---
+++ HelloRaspberry/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloRaspberry
+{
+    public partial class frmHello : Form
+    {
+        private int nClick = 0;
+        public frmHello()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClick_Click(object sender, EventArgs e)
+        {
+            RefreshText();
+        }
+
+        private void RefreshText()
+        {
+            nClick++;
+            lblText.Text = $"I've been clicked {nClick} times";
+        }
+
+    }
+}
